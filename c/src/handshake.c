3ac87e2194b9b0f65fb0f5da024253d6dcb9b14d
Christoffer Carlsson <cc@christofferc.com>
2023-07-24 01:25:22 +0200

    Add support for creating signatures externally

--- c/src/handshake.c
+++ c/src/handshake.c
@@ -7,43 +7,6 @@
 const unsigned char CONTEXT_INITIATOR = 0;
 const unsigned char CONTEXT_RESPONDER = 1;
 
-void write_transcript(unsigned char *transcript, const unsigned char *first_key,
-                      const unsigned char *second_key,
-                      const unsigned char *third_key,
-                      const unsigned char *fourth_key) {
-  memmove(transcript, first_key, 32);
-  memmove(transcript + 32, second_key, 32);
-  memmove(transcript + 64, third_key, 32);
-  memmove(transcript + 96, fourth_key, 32);
-}
-
-void calculate_transcript(unsigned char *transcript,
-                          const unsigned int is_initiator,
-                          const unsigned char *our_identity_key,
-                          const unsigned char *our_ephemeral_key,
-                          const unsigned char *their_identity_key,
-                          const unsigned char *their_ephemeral_key) {
-  if (is_initiator == 1) {
-    write_transcript(transcript, our_identity_key, their_identity_key,
-                     our_ephemeral_key, their_ephemeral_key);
-  } else {
-    write_transcript(transcript, their_identity_key, our_identity_key,
-                     their_ephemeral_key, our_ephemeral_key);
-  }
-}
-
-int calculate_ciphertext(unsigned char *message,
-                         const unsigned char *transcript,
-                         const unsigned char *our_private_key,
-                         const unsigned char *our_secret_key) {
-  unsigned char signature[64];
-  int signature_result = sign(signature, our_private_key, transcript, 128);
-  if (signature_result != 0) {
-    return -1;
-  }
-  return encrypt(message, our_secret_key, 0, signature, 64);
-}
-
 int derive_keys(unsigned char *our_secret_key, unsigned char *their_secret_key,
                 const unsigned int is_initiator, unsigned char *our_private_key,
                 const unsigned char *their_public_key) {
@@ -61,6 +24,52 @@ int derive_keys(unsigned char *our_secret_key, unsigned char *their_secret_key,
                                                                         : -1;
 }
 
+void write_transcript(unsigned char *transcript, const unsigned char *first_key,
+                      const unsigned char *second_key,
+                      const unsigned char *third_key,
+                      const unsigned char *fourth_key) {
+  memmove(transcript, first_key, 32);
+  memmove(transcript + 32, second_key, 32);
+  memmove(transcript + 64, third_key, 32);
+  memmove(transcript + 96, fourth_key, 32);
+}
+
+int autograph_transcript(unsigned char *transcript,
+                         const unsigned int is_initiator,
+                         const unsigned char *our_identity_key,
+                         const unsigned char *our_ephemeral_key,
+                         const unsigned char *their_identity_key,
+                         const unsigned char *their_ephemeral_key) {
+  if (is_initiator == 1) {
+    write_transcript(transcript, our_identity_key, their_identity_key,
+                     our_ephemeral_key, their_ephemeral_key);
+  } else {
+    write_transcript(transcript, their_identity_key, our_identity_key,
+                     their_ephemeral_key, our_ephemeral_key);
+  }
+  return 0;
+}
+
+int autograph_handshake_signature(
+    unsigned char *message, unsigned char *our_secret_key,
+    unsigned char *their_secret_key, const unsigned int is_initiator,
+    const unsigned char *our_signature,
+    unsigned char *our_private_ephemeral_key,
+    const unsigned char *their_public_ephemeral_key) {
+  int derive_result =
+      derive_keys(our_secret_key, their_secret_key, is_initiator,
+                  our_private_ephemeral_key, their_public_ephemeral_key);
+  if (derive_result != 0) {
+    return -1;
+  }
+  int ciphertext_result =
+      encrypt(message, our_secret_key, 0, our_signature, 64);
+  if (ciphertext_result != 0) {
+    return -1;
+  }
+  return 0;
+}
+
 int autograph_handshake(unsigned char *transcript, unsigned char *message,
                         unsigned char *our_secret_key,
                         unsigned char *their_secret_key,
@@ -71,19 +80,19 @@ int autograph_handshake(unsigned char *transcript, unsigned char *message,
                         const unsigned char *our_public_ephemeral_key,
                         const unsigned char *their_public_identity_key,
                         const unsigned char *their_public_ephemeral_key) {
-  calculate_transcript(transcript, is_initiator, our_public_identity_key,
-                       our_public_ephemeral_key, their_public_identity_key,
-                       their_public_ephemeral_key);
-  int derive_result =
-      derive_keys(our_secret_key, their_secret_key, is_initiator,
-                  our_private_ephemeral_key, their_public_ephemeral_key);
-  if (derive_result != 0) {
+  int transcript_result =
+      autograph_transcript(transcript, is_initiator, our_public_identity_key,
+                           our_public_ephemeral_key, their_public_identity_key,
+                           their_public_ephemeral_key);
+  if (transcript_result != 0) {
     return -1;
   }
-  int ciphertext_result = calculate_ciphertext(
-      message, transcript, our_private_identity_key, our_secret_key);
-  if (ciphertext_result != 0) {
+  unsigned char signature[64];
+  int sign_result = sign(signature, our_private_identity_key, transcript, 128);
+  if (sign_result != 0) {
     return -1;
   }
-  return 0;
+  return autograph_handshake_signature(
+      message, our_secret_key, their_secret_key, is_initiator, signature,
+      our_private_ephemeral_key, their_public_ephemeral_key);
 }
--- c/src/ownership.c
+++ c/src/ownership.c
@@ -3,18 +3,24 @@
 #include "autograph.h"
 #include "crypto.h"
 
-unsigned long long calculate_subject_size(const unsigned long long data_size) {
-  return 32 + data_size;
-}
-
-void calculate_subject(unsigned char *subject,
-                      const unsigned char *their_public_key,
-                      const unsigned char *data,
-                      const unsigned long long data_size) {
+void autograph_subject(unsigned char *subject,
+                       const unsigned char *their_public_key,
+                       const unsigned char *data,
+                       const unsigned long long data_size) {
   if (data_size > 0) {
     memmove(subject, data, data_size);
   }
   memmove(subject + data_size, their_public_key, 32);
+}
+
+unsigned long long autograph_subject_size(const unsigned long long data_size) {
+  return 32 + data_size;
+}
+
+int autograph_sign(unsigned char *signature, const unsigned char *private_key,
+                   const unsigned char *subject,
+                   const unsigned long long subject_size) {
+  return sign(signature, private_key, subject, subject_size);
 }
 
 int autograph_certify(unsigned char *signature,
@@ -22,9 +28,9 @@ int autograph_certify(unsigned char *signature,
                       const unsigned char *their_public_key,
                       const unsigned char *data,
                       const unsigned long long data_size) {
-  const unsigned long long subject_size = calculate_subject_size(data_size);
+  const unsigned long long subject_size = autograph_subject_size(data_size);
   unsigned char subject[subject_size];
-  calculate_subject(subject, their_public_key, data, data_size);
+  autograph_subject(subject, their_public_key, data, data_size);
   return sign(signature, our_private_key, subject, subject_size) == 0 ? 0 : -1;
 }
 
@@ -36,9 +42,9 @@ int autograph_verify(const unsigned char *their_public_key,
   if (certificates == NULL || certificate_count == 0) {
     return -1;
   }
-  const unsigned long long subject_size = calculate_subject_size(data_size);
+  const unsigned long long subject_size = autograph_subject_size(data_size);
   unsigned char subject[subject_size];
-  calculate_subject(subject, their_public_key, data, data_size);
+  autograph_subject(subject, their_public_key, data, data_size);
   for (unsigned long long i = 0; i < certificate_count; i++) {
     const unsigned char *certificate = certificates + i * 96;
     int verify_result =
