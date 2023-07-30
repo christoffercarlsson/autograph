b74e70fc6bf4be28ff086530fdb0caee70f0f1eb
Christoffer Carlsson <cc@christofferc.com>
2023-07-30 14:26:44 +0200

    Update XCFramework

--- swift/Clibautograph.xcframework/watchos-arm64_x86_64-simulator/Headers/autograph.h
+++ swift/Clibautograph.xcframework/watchos-arm64_x86_64-simulator/Headers/autograph.h
@@ -95,7 +95,113 @@ int autograph_verify(const unsigned char *their_public_key,
                      const unsigned long long data_size);
 
 #ifdef __cplusplus
-}
+}  // extern "C"
+
+#include <functional>
+#include <vector>
+
+namespace autograph {
+
+using Bytes = std::vector<unsigned char>;
+
+struct KeyPair {
+  Bytes private_key;
+  Bytes public_key;
+};
+
+struct KeyPairResult {
+  bool success;
+  KeyPair key_pair;
+};
+
+struct CertificationResult {
+  bool success;
+  Bytes signature;
+};
+
+struct DecryptionResult {
+  bool success;
+  Bytes data;
+};
+
+struct EncryptionResult {
+  bool success;
+  Bytes message;
+};
+
+struct SafetyNumberResult {
+  bool success;
+  Bytes safety_number;
+};
+
+using CertifyFunction = std::function<CertificationResult(const Bytes)>;
+
+using DecryptFunction = std::function<DecryptionResult(const Bytes)>;
+
+using EncryptFunction = std::function<EncryptionResult(const Bytes)>;
+
+using SafetyNumberFunction = std::function<SafetyNumberResult(const Bytes)>;
+
+using VerifyFunction = std::function<bool(const Bytes, const Bytes)>;
+
+struct Session {
+  CertifyFunction certify;
+  DecryptFunction decrypt;
+  EncryptFunction encrypt;
+  VerifyFunction verify;
+};
+
+struct SessionResult {
+  bool success;
+  Session session;
+};
+
+using SessionFunction = std::function<SessionResult(const Bytes)>;
+
+struct Handshake {
+  Bytes message;
+  SessionFunction establish_session;
+};
+
+struct HandshakeResult {
+  bool success;
+  Handshake handshake;
+};
+
+using HandshakeFunction =
+    std::function<HandshakeResult(KeyPair &, const Bytes, const Bytes)>;
+
+struct Party {
+  SafetyNumberFunction calculate_safety_number;
+  HandshakeFunction perform_handshake;
+};
+
+struct SignResult {
+  bool success;
+  Bytes signature;
+};
+
+using SignFunction = std::function<SignResult(const Bytes)>;
+
+Party create_initiator(const SignFunction sign,
+                       const Bytes identity_public_key);
+
+Party create_initiator(const KeyPair identity_key_pair);
+
+Party create_responder(const SignFunction sign,
+                       const Bytes identity_public_key);
+
+Party create_responder(const KeyPair identity_key_pair);
+
+SignFunction create_sign(const Bytes identity_private_key);
+
+KeyPairResult generate_ephemeral_key_pair();
+
+KeyPairResult generate_identity_key_pair();
+
+bool init();
+
+}  // namespace autograph
 #endif
 
 #endif
