28832bd028177dac37e65c05c8cbffc9862bcce6
Christoffer Carlsson <cc@christofferc.com>
2023-11-29 14:44:23 +0100

    Add support for Nordic nRF52 family of microcontrollers

--- cplusplus/src/core/randombytes_internal.c
+++ cplusplus/src/core/randombytes_internal.c
@@ -1,10 +1,10 @@
 #include "randombytes_internal.h"
 
-#ifdef CONFIG_IDF_TARGET
+#if __has_include("esp_system.h")
 #include "esp_system.h"
 
 static const char *autograph_randombytes_implementation_name(void) {
-  return CONFIG_IDF_TARGET;
+  return "esp_system"
 }
 
 const struct randombytes_implementation autograph_randombytes_implementation = {
@@ -13,6 +13,31 @@ const struct randombytes_implementation autograph_randombytes_implementation = {
     .stir = NULL,
     .uniform = NULL,
     .buf = esp_fill_random,
+    .close = NULL,
+};
+#elif __has_include("zephyr/random/random.h")
+#include "zephyr/random/random.h"
+
+static const char *autograph_randombytes_implementation_name(void) {
+  return "zephyr_random_random";
+}
+
+static uint32_t zephyr_random() {
+  uint32_t value;
+  sys_csrand_get(&value, sizeof(value));
+  return value;
+}
+
+static void zephyr_fill_random(void *const buf, const size_t size) {
+  sys_csrand_get(buf, size);
+}
+
+const struct randombytes_implementation autograph_randombytes_implementation = {
+    .implementation_name = autograph_randombytes_implementation_name,
+    .random = zephyr_random,
+    .stir = NULL,
+    .uniform = NULL,
+    .buf = zephyr_fill_random,
     .close = NULL,
 };
 #endif
