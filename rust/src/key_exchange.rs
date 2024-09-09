9e74943a3c7c098c6b1c9006f7bafd47a62ac9ca
Christoffer Carlsson <cc@christofferc.com>
2024-09-09 21:51:54 +0200

    Update Rust implementation

--- rust/src/key_exchange.rs
+++ rust/src/key_exchange.rs
@@ -7,8 +7,8 @@ use crate::{
 };
 use alloc::{vec, vec::Vec};
 
-pub fn create_transcript<P: DiffieHellmanPrimitive>() -> Vec<u8> {
-    let size = P::SESSION_PUBLIC_KEY_SIZE * 2;
+pub fn create_transcript<P: SigningPrimitive + DiffieHellmanPrimitive>() -> Vec<u8> {
+    let size = P::IDENTITY_PUBLIC_KEY_SIZE * 2 + P::SESSION_PUBLIC_KEY_SIZE * 2;
     vec![0; size]
 }
 
@@ -16,54 +16,61 @@ fn create_shared_secret<P: DiffieHellmanPrimitive>() -> Vec<u8> {
     vec![0; P::SHARED_SECRET_SIZE]
 }
 
-fn create_okm<P: AEADPrimitive>() -> Vec<u8> {
-    let size = P::SECRET_KEY_SIZE * 2;
-    vec![0; size]
-}
-
-fn calculate_secret_keys<P: AEADPrimitive>(is_initiator: bool, okm: &[u8]) -> (Vec<u8>, Vec<u8>) {
-    let mut sending_key = create_secret_key::<P>();
-    let mut receiving_key = create_secret_key::<P>();
-    if is_initiator {
-        sending_key.copy_from_slice(&okm[..P::SECRET_KEY_SIZE]);
-        receiving_key.copy_from_slice(&okm[P::SECRET_KEY_SIZE..]);
-    } else {
-        sending_key.copy_from_slice(&okm[P::SECRET_KEY_SIZE..]);
-        receiving_key.copy_from_slice(&okm[..P::SECRET_KEY_SIZE]);
-    }
-    (sending_key, receiving_key)
-}
-
-fn derive_secret_keys<P: DiffieHellmanPrimitive + KeyDerivationPrimitive + AEADPrimitive>(
-    is_initiator: bool,
-    our_session_key_pair: &[u8],
-    their_session_key: &[u8],
-) -> Result<(Vec<u8>, Vec<u8>), Error> {
-    let mut shared_secret = create_shared_secret::<P>();
-    let mut okm = create_okm::<P>();
-    let dh_success = P::diffie_hellman(&mut shared_secret, our_session_key_pair, their_session_key);
-    let kdf_success = P::kdf(&mut okm, &shared_secret);
-    let (sending_key, receiving_key) = calculate_secret_keys::<P>(is_initiator, &okm);
-    if dh_success && kdf_success {
-        Ok((sending_key, receiving_key))
+fn derive_key<P: KeyDerivationPrimitive + AEADPrimitive>(
+    shared_secret: &[u8],
+    transcript: &[u8],
+    initiator: bool,
+) -> Result<Vec<u8>, Error> {
+    let mut key = create_secret_key::<P>();
+    let mut info = vec![if initiator { 1 } else { 0 }; 1];
+    info.extend_from_slice(transcript);
+    let success = P::kdf(&mut key, shared_secret, &info);
+    if success {
+        Ok(key)
     } else {
         Err(Error::KeyExchange)
     }
 }
 
-fn calculate_transcript<P: DiffieHellmanPrimitive>(
+fn derive_secret_keys<P: DiffieHellmanPrimitive + KeyDerivationPrimitive + AEADPrimitive>(
     is_initiator: bool,
+    transcript: &[u8],
     our_session_key_pair: &[u8],
     their_session_key: &[u8],
+) -> Result<(Vec<u8>, Vec<u8>), Error> {
+    let mut shared_secret = create_shared_secret::<P>();
+    if !P::diffie_hellman(&mut shared_secret, our_session_key_pair, their_session_key) {
+        return Err(Error::KeyExchange);
+    }
+    let a = derive_key::<P>(&shared_secret, transcript, true)?;
+    let b = derive_key::<P>(&shared_secret, transcript, false)?;
+    if is_initiator {
+        Ok((a, b))
+    } else {
+        Ok((b, a))
+    }
+}
+
+fn calculate_transcript<P: SigningPrimitive + DiffieHellmanPrimitive>(
+    is_initiator: bool,
+    our_identity_key_pair: &[u8],
+    our_session_key_pair: &[u8],
+    their_identity_key: &[u8],
+    their_session_key: &[u8],
 ) -> Vec<u8> {
-    let mut transcript = create_transcript::<P>();
+    let mut transcript = Vec::new();
+    let our_identity_key = get_identity_public_key::<P>(our_identity_key_pair);
     let our_session_key = get_session_public_key::<P>(our_session_key_pair);
     if is_initiator {
-        transcript[..P::SESSION_PUBLIC_KEY_SIZE].copy_from_slice(&our_session_key);
-        transcript[P::SESSION_PUBLIC_KEY_SIZE..].copy_from_slice(their_session_key);
+        transcript.extend_from_slice(&our_identity_key);
+        transcript.extend_from_slice(&our_session_key);
+        transcript.extend_from_slice(their_identity_key);
+        transcript.extend_from_slice(their_session_key);
     } else {
-        transcript[..P::SESSION_PUBLIC_KEY_SIZE].copy_from_slice(their_session_key);
-        transcript[P::SESSION_PUBLIC_KEY_SIZE..].copy_from_slice(&our_session_key);
+        transcript.extend_from_slice(their_identity_key);
+        transcript.extend_from_slice(their_session_key);
+        transcript.extend_from_slice(&our_identity_key);
+        transcript.extend_from_slice(&our_session_key);
     }
     transcript
 }
@@ -81,11 +88,20 @@ pub fn key_exchange<
     their_identity_key: &[u8],
     their_session_key: &[u8],
 ) -> Result<(Transcript, Signature, SecretKey, SecretKey), Error> {
-    let transcript =
-        calculate_transcript::<P>(is_initiator, our_session_key_pair, their_session_key);
+    let transcript = calculate_transcript::<P>(
+        is_initiator,
+        our_identity_key_pair,
+        our_session_key_pair,
+        their_identity_key,
+        their_session_key,
+    );
     let signature = certify::<P>(our_identity_key_pair, their_identity_key, Some(&transcript))?;
-    let (sending_key, receiving_key) =
-        derive_secret_keys::<P>(is_initiator, our_session_key_pair, their_session_key)?;
+    let (sending_key, receiving_key) = derive_secret_keys::<P>(
+        is_initiator,
+        &transcript,
+        our_session_key_pair,
+        their_session_key,
+    )?;
     Ok((transcript, signature, sending_key, receiving_key))
 }
 
